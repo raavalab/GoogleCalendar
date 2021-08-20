7e0657ac8506156ed90671e46ca38aafbba1447b
raava <raavalab@gmail.com>
2021-08-20 00:30:49 -0300

    23

--- index.js
+++ index.js
@@ -22,30 +22,6 @@ app.get('/', (req, res) => {
 
 app.post('/webhook', async (req, res) => {
     
-    const mensagem = req.body.queryResult.queryText;
-    const intencao = req.body.queryResult.intent.displayName;
-
-    switch(intencao) {
-        case 'Default Welcome Intent':
-            break;
-        case 'verCardapio':
-            break;
-        case 'verStatus':
-            break;
-        case 'marcarConsulta':
-          break;
-        case 'verAgenda':
-            break;
-        case 'salvarPlanilha':
-            break;
-        case 'verPlanilha':
-            break;
-        case 'teste':
-              break;
-        default: 
-    }
-    
-
 })
 
 const porta = process.env.PORT || 8080;
